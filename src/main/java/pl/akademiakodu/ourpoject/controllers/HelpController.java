973b0a7860321fc6c69c73258bf5a8f4029b1d9a
Kerawos <Maresowa@gmail.com>
2017-09-01 12:19:52 +0200

    add 'userExist' in HelpController

--- src/main/java/pl/akademiakodu/ourpoject/controllers/HelpController.java
+++ src/main/java/pl/akademiakodu/ourpoject/controllers/HelpController.java
@@ -1,5 +1,17 @@
 package pl.akademiakodu.ourpoject.controllers;
 
 public class HelpController {
-    
+
+    //check if user exist (parameter 'name' - will be checking)
+    @RequestMapping(path = "/userexist{name}", method = RequestMethod.GET)
+    @ResponseBody
+    public String userExist(@PathVariable("name") String name){
+        if(isUserExist(name)){
+            return "<h2><center>Uzytkownik " + name + " istnieje </center></h2>";
+        } else {
+            return "<h2><center>Niestety nie mamy zapisanego uzytkownika " + name + " ... </center></h2>";
+        }
+    }
+
+
 }
