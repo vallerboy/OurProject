fb70aeb1263257fefbe1c2c9e76c998f6a8fd57c
waluus9 <waluus9@gmail.com>
2017-09-01 13:07:19 +0200

    index template +register

--- src/main/java/pl/akademiakodu/ourpoject/controllers/HelpController.java
+++ src/main/java/pl/akademiakodu/ourpoject/controllers/HelpController.java
@@ -8,16 +8,16 @@ import org.springframework.web.bind.annotation.*;
 
 public class HelpController {
 
-    check if user exist (parameter 'name' - will be checking)
-    @RequestMapping(path = "/userexist{name}", method = RequestMethod.GET)
-    @ResponseBody
-    public String userExist(@PathVariable("name") String name){
-        if(isUserExist(name)){
-            return "<h2><center>Uzytkownik " + name + " istnieje </center></h2>";
-        } else {
-            return "<h2><center>Niestety nie mamy zapisanego uzytkownika " + name + " ... </center></h2>";
-        }
-    }
+//    check if user exist (parameter 'name' - will be checking)
+//    @RequestMapping(path = "/userexist{name}", method = RequestMethod.GET)
+//    @ResponseBody
+//    public String userExist(@PathVariable("name") String name){
+//        if(isUserExist(name)){
+//            return "<h2><center>Uzytkownik " + name + " istnieje </center></h2>";
+//        } else {
+//            return "<h2><center>Niestety nie mamy zapisanego uzytkownika " + name + " ... </center></h2>";
+//        }
+//    }
 
     //method checking user exists?
     public static boolean isUserExist(String name){
