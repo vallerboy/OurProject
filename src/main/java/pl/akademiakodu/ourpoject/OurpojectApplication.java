edd4887e31769b7b0f1282d464fb05554a87f4ee
Oskar Polak <oskarbank@gmail.com>
2017-09-01 11:56:19 +0200

    Init with spring

---
+++ src/main/java/pl/akademiakodu/ourpoject/OurpojectApplication.java
@@ -0,0 +1,12 @@
+package pl.akademiakodu.ourpoject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OurpojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OurpojectApplication.class, args);
+	}
+}
---
+++ src/test/java/pl/akademiakodu/ourpoject/OurpojectApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.akademiakodu.ourpoject;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OurpojectApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
