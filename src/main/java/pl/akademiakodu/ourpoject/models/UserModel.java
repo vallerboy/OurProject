e9e99223db94a88005c749cda4942b153e44db4e
Robert Jalocha <robertj82@interia.pl>
2017-09-01 12:19:05 +0200

    UserModel

--- src/main/java/pl/akademiakodu/ourpoject/models/UserModel.java
+++ src/main/java/pl/akademiakodu/ourpoject/models/UserModel.java
@@ -1,17 +1,23 @@
 package pl.akademiakodu.ourpoject.models;
 
 public class UserModel {
-    String name;
-    String surname;
-    String password;
+   private String name;
+   private String surname;
+   private String email;
+    private String login;
+   private String password;
+   private String repeatPassword;
 
     public UserModel() {
     }
 
-    public UserModel(String name, String surname, String password) {
+    public UserModel(String name, String surname, String email, String login, String password, String repeatPassword) {
         this.name = name;
         this.surname = surname;
+        this.email = email;
+        this.login = login;
         this.password = password;
+        this.repeatPassword = repeatPassword;
     }
 
     public String getName() {
@@ -30,11 +36,35 @@ public class UserModel {
         this.surname = surname;
     }
 
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
     public String getPassword() {
         return password;
     }
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public String getRepeatPassword() {
+        return repeatPassword;
+    }
+
+    public void setRepeatPassword(String repeatPassword) {
+        this.repeatPassword = repeatPassword;
     }
 }
